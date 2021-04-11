6dc5aa2f65e66f9796a9803e5a6ac04efb686edc
Kubirill <Kubirill@rambler.ru>
2021-04-11 13:36:23 +0300

    FirstPortal

---
+++ Assets/Scripts/CameraScale.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScale : MonoBehaviour
+{
+    public Transform insideObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = Vector3.one / insideObj.localScale.x;
+    }
+}
